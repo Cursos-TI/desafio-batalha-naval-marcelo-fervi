463a668d87bec771f5f898b652dee41d45bde0fd
Marcelo Ferreira <35692178+marcelo-fervi@users.noreply.github.com>
2025-02-15 16:23:22 +0000

    Progresso no Desafio Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,13 +2,38 @@
 
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+
+const int tabuleiroTamanhoX = 5, tabuleiroTamanhoY = 5;
+
+static void posicionarNavioEm(int tabuleiro[tabuleiroTamanhoX][tabuleiroTamanhoY], int posicaoX, int posicaoY, int larguraX, int alturaY) {
+    int posicaoFinalX = posicaoX + larguraX;
+    int posicaoFinalY = posicaoY + alturaY;
+    for (int y = posicaoY; y < posicaoFinalY; y++) {
+        for (int x = posicaoX; x < posicaoFinalX; x++) {
+            tabuleiro[y][x] = 3; //Indicador de presença de navio
+        }
+    }
+}
+
+static void exibirTabuleiro(int tabuleiro[tabuleiroTamanhoX][tabuleiroTamanhoY]) {
+    for (int x = 0; x < tabuleiroTamanhoX; x++) {
+        for (int y = 0; y < tabuleiroTamanhoY; y++) {
+            printf("%d ", tabuleiro[x][y]);
+        }
+
+        printf("\n");
+    }
+}
 
 int main() {
     // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int tabuleiro[tabuleiroTamanhoX][tabuleiroTamanhoY] = {};
+    
+    posicionarNavioEm(tabuleiro, 0, 0, 5, 1); //Navio posicionado horizontalmente
+    posicionarNavioEm(tabuleiro, 4, 1, 1, 4); //Navio posicionado verticalmente
+
+    printf("Tabuleiro atual:\n");
+    exibirTabuleiro(tabuleiro);
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -25,7 +50,7 @@ int main() {
     // 0 0 1 0 0
     // 0 1 1 1 0
     // 1 1 1 1 1
-    
+
     // Exemplo para habilidade em octaedro:
     // 0 0 1 0 0
     // 0 1 1 1 0
